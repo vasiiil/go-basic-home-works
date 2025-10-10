c4db0a8818cac127f6821a4657e051be7d2317b4
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2025-10-10 17:34:59 +0300

    Дописал конвертер

--- dz-1-converter/main.go
+++ dz-1-converter/main.go
@@ -1,25 +1,121 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+const EMPTY_CURRENCY_ERROR_CODE = "EMPTY_VALUE"
+const UNKNOWN_CURRENCY_ERROR_CODE = "UNKNOWN_VALUE"
+const EXCLUDE_CURRENCY_ERROR_CODE = "EXCLUDE_VALUE"
 
 func main() {
-
-	eur := getUserInput()
-	rub := convert(eur, "eur", "rub")
-	fmt.Printf("%.2f", rub)
+	sourceCurrecyCode := getCurrency(false, "")
+	value := getUserInput()
+	targerCurrecyCode := getCurrency(true, sourceCurrecyCode)
+	convertedValue, _ := convert(value, sourceCurrecyCode, targerCurrecyCode)
+	fmt.Printf("%.2f", convertedValue)
 }
 
 func getUserInput() float64 {
 	var value float64
 	fmt.Print("Введите число: ")
-	fmt.Scan(&value)
+	for {
+		fmt.Scan(&value)
+		if value <= 0 {
+			fmt.Println("Введите нормальное число")
+			continue
+		}
+		break
+	}
 
 	return value
 }
 
-func convert(value float64, source string, target string) float64 {
+func getCurrency(isTarget bool, excludeCurrencyCode string) string {
+	currencies := make([]string, 0, 3)
+	if excludeCurrencyCode != "u" {
+		currencies = append(currencies, "USD (U)")
+	}
+	if excludeCurrencyCode != "e" {
+		currencies = append(currencies, "EUR (E)")
+	}
+	if excludeCurrencyCode != "r" {
+		currencies = append(currencies, "RUB (R)")
+	}
+
+	currencyType := "исходную"
+	if isTarget {
+		currencyType = "конечную"
+	}
+	fmt.Println("Выберите", currencyType, "валюту:", strings.Join(currencies, ", "))
+
+	for {
+		currencyCode, err := getUserCurrency(excludeCurrencyCode)
+		if err != nil {
+			errorCode := err.Error()
+			if errorCode == EMPTY_CURRENCY_ERROR_CODE {
+				fmt.Println("Необходимо выбрать валюту")
+				continue
+			}
+			if errorCode == UNKNOWN_CURRENCY_ERROR_CODE {
+				fmt.Println("Такой валюты нет")
+				continue
+			}
+			if errorCode == EXCLUDE_CURRENCY_ERROR_CODE {
+				fmt.Println("Нельзя выбирать ту же валюту")
+				continue
+			}
+		}
+		return currencyCode
+	}
+}
+
+func getUserCurrency(excludeCurrencyCode string) (string, error) {
+	var currencyCode string
+	fmt.Scan(&currencyCode)
+	if currencyCode == "" {
+		return "", errors.New(EMPTY_CURRENCY_ERROR_CODE)
+	}
+	currencyCode = strings.ToLower(currencyCode)
+	if currencyCode != "u" && currencyCode != "e" && currencyCode != "r" {
+		return "", errors.New(UNKNOWN_CURRENCY_ERROR_CODE)
+	}
+	if currencyCode == excludeCurrencyCode {
+		return "", errors.New(EXCLUDE_CURRENCY_ERROR_CODE)
+	}
+	return currencyCode, nil
+}
+
+func convert(value float64, source string, target string) (float64, error) {
 	const usdToEur float64 = 1.4
-	const usdToRub float64 = 94
-	const eurToRub = usdToRub * usdToEur
-	return value * eurToRub
+	const rubToUsd float64 = 94
+	const eurToRub = usdToEur * rubToUsd
+
+	switch source {
+	case "u":
+		switch target {
+		case "e":
+			return value / usdToEur, nil
+		case "r":
+			return value / rubToUsd, nil
+		}
+	case "e":
+		switch target {
+		case "u":
+			return value / usdToEur, nil
+		case "r":
+			return value * eurToRub, nil
+		}
+	case "r":
+		switch target {
+		case "u":
+			return value * rubToUsd, nil
+		case "e":
+			return value / eurToRub, nil
+		}
+	}
+
+	return 0, errors.New("UNKNOWN_CURRENCIES")
 }
