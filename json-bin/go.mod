3fe98feeefd5464e99658005ab9d5cd6ffadf52e
Vasiliy Nikitin <nikitin-vasya95@yandex.ru>
2025-10-14 15:19:18 +0300

    Инит проекта с Json Bin

---
+++ json-bin/go.mod
@@ -0,0 +1,3 @@
+module json-bin
+
+go 1.25.1
---
+++ json-bin/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "time"
+
+type Bin struct {
+	id        string
+	private   bool
+	createdAt time.Time
+	name      string
+}
+
+func newBin(id string, private bool, createdAt time.Time, name string) *Bin {
+	return &Bin{
+		id:        id,
+		private:   private,
+		createdAt: createdAt,
+		name:      name,
+	}
+}
+
+type BinList = []Bin
+
+func main() {}
